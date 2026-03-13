0a8f2c5e2de0f5d4da57e0692552769eb2fb537a
ParvaKumar <p4rz1v4lprogramming@gmail.com>
2026-03-13 17:26:19 +0530

    implemented monitoring endpoints for health, runtime info, and environment details in Express server

---
+++ session3(express-REST-middleware)/express-intro/healthCheck.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.get('/health', (req, res) => {
+    res.json({
+        status: "OK",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+    });
+});
+
+app.get('/info', (req, res) => {
+    res.json({
+        node: process.version,
+        platform: process.platform
+    });
+});
+
+app.get("/env", (req, res) => {
+    res.json({
+        environment: process.env.NODE_ENV || "developement"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
